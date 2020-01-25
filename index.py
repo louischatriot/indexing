5acca22f1ba08a7a0dd3c0870b829b39ec839455
Louis Chatriot <louis.chatriot@gmail.com>
2020-01-25 22:15:47 +0100

    Can test with a large database in CSV format

--- index.py
+++ index.py
@@ -2,9 +2,10 @@ from timing import time_execution
 from mem import print_memory_usage
 from fixtures import generate_word, generate_number
 
-print_memory_usage()
 
-fields = ["email", "phone", "customer.card.fingerprint"]
+# Add id here ; whenever a new doc is added, check whether it's already in the database
+# in which case update it if needed as well as indexes
+fields = ["email", "phone", "fingerprint"]
 indexes = {}
 objects = []
 
@@ -33,39 +34,32 @@ def register_object(o):
 
 
 
-ind = {}
-address_ind = {}
 
-N = 10000
+# N = 10000
+# with time_execution("Creating database"):
+    # for i in range(0,N):
+        # doc = {
+            # "id": f"id_{generate_word()}",
+            # "email": generate_word(),
+            # "phone": generate_number(18, 65),
+            # "customer.card.fingerprint": generate_word(),
+            # "is_fraud": bool(generate_number(0, 100) > 50),
+        # }
+        # register_object(doc)
 
-with time_execution("Creating database"):
-    for i in range(0,N):
-        doc = {
-            "id": f"id_{generate_word()}",
-            "email": generate_word(),
-            "phone": generate_number(18, 65),
-            "customer.card.fingerprint": generate_word(),
-            "is_fraud": bool(generate_number(0, 100) > 50),
-        }
-        register_object(doc)
+# print_memory_usage()
 
-print_memory_usage()
-
-some_object = objects[generate_number(0, N)]
-some_email = some_object["email"]
+# some_object = objects[generate_number(0, N)]
+# some_email = some_object["email"]
 
 
-print(some_email)
+# N = 100000
+# res = 0
 
-N = 100000
-res = 0
+# with time_execution(f"Searching {N} times"):
+    # for i in range(0,N):
+        # res += len(get_from_index("email", some_email))
 
-with time_execution(f"Searching {N} times"):
-    for i in range(0,N):
-        res += len(get_from_index("email", some_email))
-
-print_memory_usage()
-print(res)
 
 
 
---
+++ load_data.py
@@ -0,0 +1,36 @@
+from timing import time_execution
+from mem import print_memory_usage
+from index import register_object, get_from_index
+import csv
+
+filename = "NONE"
+
+headers = None
+
+with time_execution("Loading database in index"):
+    with open(filename, newline='') as csvfile:
+        spamreader = csv.reader(csvfile)
+        for row in spamreader:
+            if headers is None:
+                headers = row
+            else:
+                o = {}
+                for i in range(0, len(headers)):
+                    o[headers[i]] = row[i]
+
+                register_object(o)
+
+print_memory_usage()
+
+
+N = 100000
+res = 0
+some_email = ""
+
+with time_execution(f"Searching {N} times"):
+    for i in range(0,N):
+        res += len(get_from_index("email", some_email))
+
+print(len(get_from_index("email", some_email)))
+
+
